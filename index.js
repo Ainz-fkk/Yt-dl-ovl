267267fafd2d19b62bf6a2985f53883279205e3d
Ainz-fkk <ainzgit@outlook.fr>
2025-04-02 20:48:41 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 8000;
+const ytdlRoute = require('./ytdl');
 
+app.use(cors());  
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/ovl-yt-dl', ytdl);
-
+app.use(ytdlRoute);
 
 app.listen(PORT, () => {
     console.log(`Serveur en cours d'exécution sur http://localhost:${PORT}`);
 });
 
 module.exports = app;
-
